a5bac1a36b17d31e148a7acd2bcc0a8264f5d2f6
Jordan-Tay <tayjordan25@gmail.com>
2020-09-26 03:25:56 +0800

    Updates

---
+++ precache-manifest.33201639e63588bd54a62540fd1bc7e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8f759ef922b6f4186a84dc89ddfde6",
+    "url": "/pages/index.html"
+  },
+  {
+    "revision": "074e2c672c2a0fe252aa",
+    "url": "/pages/static/css/main.e8c272f0.chunk.css"
+  },
+  {
+    "revision": "698fb8044f372683cc7d",
+    "url": "/pages/static/js/2.f9da9472.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/pages/static/js/2.f9da9472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074e2c672c2a0fe252aa",
+    "url": "/pages/static/js/main.bea01e23.chunk.js"
+  },
+  {
+    "revision": "b9873beeb7cc12688bbb",
+    "url": "/pages/static/js/runtime-main.7223f343.js"
+  }
+]);
